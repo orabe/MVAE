b9bd5a040212ebc2958bc7071692546f59db9767
Mohammad <orabe.mhd@gmail.com>
2024-01-04 02:30:21 +0100

    (sampling) clean up

--- src/mnist/sample.py
+++ src/mnist/sample.py
@@ -1,63 +1,3 @@
-"""import torch
-import matplotlib.pyplot as plt
-import os
-
-from model import MVAE
-
-def sample_from_mvae(model, n_samples):
-    model.eval() 
-    with torch.no_grad():
-        # Generate random samples from a standard normal distribution
-        z_samples = torch.randn(n_samples, model.z_dim)
-
-        # Decode the samples
-        gen_img = model.image_decoder(z_samples)
-        gen_lbl = model.label_decoder(z_samples)
-
-        return gen_img, gen_lbl
-
-def plot_generated_samples(images, labels):
-    _, axs = plt.subplots(4, 4)
-    axs = axs.flatten()
-
-    for i in range(images.size(0)):
-        img = images[i].view(28, 28).numpy()
-        axs[i].imshow(img, cmap='gray')
-        axs[i].axis('off')
-        axs[i].set_title(f"Label: {torch.argmax(labels[i]).item()}")
-
-    plt.show()
-
-# Load the trained model
-# ---------------------------------
-# the following works only on colab
-
-# from google.colab import drive
-# drive.mount('/content/gdrive/')
-
-save_path = '/content/weights/'
-# if not os.path.exists(save_path):
-#     os.makedirs(save_path)
-
-# use this before training to save the weights 
-# save_interval = 10 # save weights every 10 epochs
-model_path = save_path
-# ---------------------------------
-
-default_values = {"n_lat_dim": 64, "lr": 1e-3, "n_epochs": 500, "n_first_annealing_epochs": 200}
-loaded_model = MVAE(default_values["n_lat_dim"])
-loaded_model.load_state_dict(torch.load(model_path))
-loaded_model.eval()
-
-# Sample from the loaded model
-generated_images, generated_labels = sample_from_model(loaded_model, n_samples=16)
-
-# Plot the generated samples
-plot_generated_samples(generated_images, generated_labels)
-
-"""
-
-
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -69,7 +9,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from model import MVAE
-
 
 
 def fetch_mnist_image(label):
